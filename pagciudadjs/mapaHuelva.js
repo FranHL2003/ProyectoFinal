7577f4de4c8cffcf3100ab7d442016799bbb7891
FranHL2003 <francisco.s235304@cesurformacion.com>
2023-05-08 16:52:52 +0200

    changes

---
+++ pagciudadjs/mapaHuelva.js
@@ -0,0 +1,80 @@
+
+
+var map = L.map('mapa').setView([37.2639, -6.9343], 14);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom: 14,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+var lista = []
+var elementoSeleccionado = 0;
+
+
+
+function cargarModal(){
+    let elemento = lista[0]
+
+}
+
+fetch("http://localhost/Proyecto%20fin%20de%20grado/test.php?province=Huelva")
+    .then(res => res.json())
+    .then(data => {
+        const tbody = document.querySelector("#listaElementos");
+        const template = document.querySelector('#elementoLista');
+        for (let i = 0; i < data.length; i++) {
+            // Clone the new row and insert it into the table
+            const clone = template.content.cloneNode(true);
+            let ciudad =clone.getElementById("ciudad")
+            let nombre = clone.getElementById('nombre')
+            let horario = clone.getElementById('horario')
+            let categoria = clone.getElementById('categoria')
+            let direccion = clone.getElementById('direccion')
+            let telefono = clone.getElementById('telefono')
+            nombre.textContent = data[i].nombre;
+            horario.textContent = data[i].horario;
+            categoria.textContent = data[i].categoria;
+            direccion.textContent = data[i].direccion;
+            telefono.textContent = data[i].telefono;
+            ciudad.textContent = data[i].ciudad;
+            if (telefono.textContent != "") {
+                telefono.classList.add('tarjeta')
+            }
+            tbody.appendChild(clone);
+
+            let elemento = {
+                nombre: data[i].nombre,
+                horario: data[i].horario,
+                categoria: data[i].categoria,
+                direccion: data[i].direccion,
+                telefono: data[i].telefono,
+                ciudad: data[i].ciudad,
+
+            }
+            lista.push(elemento)
+            
+            template.addEventListener('click', 
+                cargarModal()
+            )
+
+            var marcador = L.marker([data[i].x, data[i].y]).addTo(map);
+            marcador.bindPopup(`${data[i].nombre}`);
+
+        }
+
+
+    })
+
+
+
+
+
+
+   // http://localhost/Proyecto%20fin%20de%20grado/test.php?province=jaen
+
+    
+
+
+
+
+
