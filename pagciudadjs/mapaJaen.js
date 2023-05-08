c405a6d2f5d13f5bc2383b807ce5800045d7d3b4
FranHL2003 <francisco.s235304@cesurformacion.com>
2023-05-08 15:32:27 +0200

    copia

--- pagciudadjs/mapaJaen.js
+++ pagciudadjs/mapaJaen.js
@@ -1,6 +1,6 @@
 
 
-var map = L.map('mapa').setView([37.7716, -3.7870], 15);
+var map = L.map('mapa').setView([37.7724, -3.7862], 15);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     minZoom: 14,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -14,24 +14,10 @@ var elementoSeleccionado = 0;
 
 function cargarModal(){
     let elemento = lista[0]
-    let nombre = document.getElementById('nombreElemento')
-    let direccion = document.getElementById('direccionElemento')
-    let horario = document.getElementById('horarioElemento')
-    let telefono = document.getElementById('telefonoElemento')
-    let categoria = document.getElementById('categoriaElemento')
-    direccion.classList.add('mb-2')
-    horario.classList.add('mb-2')
-    telefono.classList.add('mb-2')
-    categoria.classList.add('mb-2')
-    nombre.textContent = elemento.nombre
-    direccion.textContent = elemento.direccion
-    horario.textContent = elemento.horario
-    telefono.textContent = elemento.telefono
-    categoria.textContent=elemento.categoria
+
 }
 
-
-fetch("jaen.json")
+fetch("http://localhost/Proyecto%20fin%20de%20grado/test.php?province=Jaen")
     .then(res => res.json())
     .then(data => {
         const tbody = document.querySelector("#listaElementos");
@@ -39,18 +25,18 @@ fetch("jaen.json")
         for (let i = 0; i < data.length; i++) {
             // Clone the new row and insert it into the table
             const clone = template.content.cloneNode(true);
+            let ciudad =clone.getElementById("ciudad")
             let nombre = clone.getElementById('nombre')
             let horario = clone.getElementById('horario')
-            let categoria=clone.getElementById('categoria')
+            let categoria = clone.getElementById('categoria')
             let direccion = clone.getElementById('direccion')
             let telefono = clone.getElementById('telefono')
-            let ver = clone.getElementById('ver')
             nombre.textContent = data[i].nombre;
             horario.textContent = data[i].horario;
+            categoria.textContent = data[i].categoria;
             direccion.textContent = data[i].direccion;
             telefono.textContent = data[i].telefono;
-            categoria.textcontent=data[i].categoria
-            ver.textcontent=data[i].ver
+            ciudad.textContent = data[i].ciudad;
             if (telefono.textContent != "") {
                 telefono.classList.add('tarjeta')
             }
@@ -59,10 +45,11 @@ fetch("jaen.json")
             let elemento = {
                 nombre: data[i].nombre,
                 horario: data[i].horario,
+                categoria: data[i].categoria,
                 direccion: data[i].direccion,
                 telefono: data[i].telefono,
-                categoria: data[i].categoria,
-                ver: data[i].ver,
+                ciudad: data[i].ciudad,
+
             }
             lista.push(elemento)
             
@@ -74,8 +61,16 @@ fetch("jaen.json")
             marcador.bindPopup(`${data[i].nombre}`);
 
         }
+
+
     })
 
+
+
+
+
+
+   // http://localhost/Proyecto%20fin%20de%20grado/test.php?province=jaen
 
     
 
